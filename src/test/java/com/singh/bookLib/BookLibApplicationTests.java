586dc0eb6175afa6244123bee73948f8d2a0ecad
Tejendra Pratap Singh <tejendrasingh104@gmail.com>
2024-06-19 21:18:01 +0530

    starting file

---
+++ src/test/java/com/singh/bookLib/BookLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singh.bookLib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
